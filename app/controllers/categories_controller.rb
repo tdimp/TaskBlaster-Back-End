4b06874bcee6ae364ee1a3f6231f9606d610b5d4
Thomas Dimperio <twdimperio@gmail.com>
2022-05-19 20:06:52 -0600

    add get Category by id functionality to CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,13 @@
 class CategoriesController < ApplicationController
+  
   get '/categories' do
     categories = Category.all
     categories.to_json
   end
+
+  get '/categories/:id' do
+    category = Category.find(params[:id])
+    category.to_json
+  end
+
 end
