a416b5caf4b5d0620464315606df57faa76716fd
Thomas Dimperio <twdimperio@gmail.com>
2022-05-21 14:16:00 -0600

    add CRUD capability to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
 class UsersController < ApplicationController
+  
   get '/users' do
     users = User.all
     users.to_json
+  end
+
+  get '/users/:id' do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
+  post '/users' do
+    user = User.new(params[:user])
+    if user.save
+      user.to_json
+    else
+      { errors: user.errors.full_messages }.to_json
+    end
+  end
+
+  patch '/users/:id' do
+    user = User.find(params[:id])
+    if user && user.update(params[:user])
+      user.to_json
+    else
+      { errors: user.errors.full_messages }.to_json
+    end
+  end
+
+  delete '/user/:id' do
+    user = User.find(params[:id])
+    user.destroy
+    user.to_json
   end
 end
