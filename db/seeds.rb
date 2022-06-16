fa93eb373b5c22996376d567e114b90f386269b5
Thomas Dimperio <twdimperio@gmail.com>
2022-06-16 11:32:05 -0600

    Change seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,74 @@ require "date"
 
 puts "🌱 Seeding spices..."
 
-3.times do
-  user = User.create(name: Faker::Name.name)
 
-  rand(1..3).times do
-    task = Task.create(
-      name: Faker::Lorem.word,
-      description: Faker::Lorem.sentence,
-      category_id: rand(1..3),
-      user_id: rand(1..3),
-      deadline: Date.new(2022,12,31),
-      priority: ["High", "Medium", "Low"].sample
-    )
-  end
-end
+User.create(name: "John")
+User.create(name: "Mike")
+User.create(name: "Dan")
+
+Task.create(
+    {name: "Mow Lawn",
+    description: "Mow front and back lawns, trim hedges",
+    category_id: 1,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Make Dinner",
+    description: "Thaw chicken, chop veggies, make stew",
+    category_id: 1,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Get groceries",
+    description: "Buy produce, snacks, and supplies",
+    category_id: 3,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Pick up drycleaning",
+    description: "Cleaners on 4th Ave",
+    category_id: 3,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Feed dogs",
+    description: nil,
+    category_id: 1,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "TPS Reports",
+    description: "Finish TPS reports, send to Bill",
+    category_id: 2,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
+Task.create(
+    {name: "Staff Meeting",
+    description: nil,
+    category_id: 2,
+    user_id: rand(1..3),
+    deadline: Date.new(2022,12,31),
+    priority: ["High", "Medium", "Low"].sample}
+)
+
 
 Category.create(
   name: "Home"
